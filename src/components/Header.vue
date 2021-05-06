19cff272358645b50bb807a1c6d266776dea16ce
MattisLm <matthislm@hotmail.com>
2021-05-06 15:24:38 +0200

    premier jet header

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<style>
+  nav {
+    padding: 16px 24px;
+  }
+  .header-app {
+    height: 80px;
+  }
+</style>
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light header-app">
+    <a class="navbar-brand" href="#">LastMusic</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Lien</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" >Lien</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login/Register</a>
+      </li>
+    </ul>
+  </div>
+   <a class="nav-link" href="#">Login/Register</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  </header>
+</template>
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,38 @@
 <template>
   <div>
-    <h1> Login </h1>
-    <label>Login</label>
-    <input v-model="email"/>
-    <label>Password</label>
-    <input type="password" v-model="password"/>
-    <button @click="submitLogin">Connexion</button>
+  <header-app/>
+    <div class="container">
+     <div class="row">
+     <div class="col"></div>
+     <div class="col-12 col-lg-5">
+      <h1 class="my-5"> Se connecter </h1>
+    <form>
+    <div class="form-group mb-3">
+      <label>Adresse mail</label>
+      <input type="email" class="form-control"  placeholder="Enter email" v-model="email"/>
+    </div>
+    <div class="form-group">
+      <label>Mot de Passe</label>
+      <input type="password" class="form-control" placeholder="Mot de passe" v-model="password"/>
+    </div>
+      <button type="submit" class="btn btn-primary my-2" @click="submitLogin">Connexion</button>
+    </form>
+      </div>
+      <div class="col"></div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import jwt_decode from "jwt-decode"
+import Header from '@/components/Header'
+
 export default {
+  components: {
+    'header-app': Header,
+  },
   data () {
     return {
       email: null,
